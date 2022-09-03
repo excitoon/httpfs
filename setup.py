ccc37d144ddf311a44e0013238519bb775fed4b3
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2022-09-03 16:36:39 +0300

    Fixed wrong package.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(f"{os.path.dirname(os.path.abspath(__file__))}/requirements.txt") as r
     with open(f"{os.path.dirname(os.path.abspath(__file__))}/README.md") as readme:
         setuptools.setup(
             name="httpfs-py",
-            version="0.0.1",
+            version="0.0.2",
             description="written in Python",  # FIXME
             long_description=readme.read(),
             long_description_content_type="text/markdown",
